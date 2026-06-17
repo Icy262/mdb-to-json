a67703c14aab7a68e59b478fe05975f7a476ff80
Icy26 <114495528+Icy262@users.noreply.github.com>
2026-06-17 08:57:16 -0400

    Fix cors headers, status codes, and json construction in api response. Fix formatting

--- src/index.ts
+++ src/index.ts
@@ -9,43 +9,32 @@ const __dirname = path.dirname(__filename)
 
 const app = express()
 
-const corsHeaders = {
-	"Access-Control-Allow-Origin": "*",
-	"Access-Control-Allow-Methods": "POST, OPTIONS",
-	"Access-Control-Allow-Headers": "Content-Type",
-};
-
 // Example API endpoint - JSON
 app.post('/get-tables', (req, res) => {
 	if (req.method == 'POST') {
-			try {
-				express.raw({ limit: '50mb' });
-				const reader = new MDBReader(req.body);
+		try {
+			express.raw({ limit: '50mb' });
+			const reader = new MDBReader(req.body);
 
-				res.json({
-					status: 200,
-					headers: corsHeaders,
-					tables: reader?.getTableNames()
-				});
-			} catch (err: unknown) {
-				res.json({
-					status: 400,
-					headers: corsHeaders,
-					message: `Failed to read .mdb file: ${err instanceof Error ? err.message : String(err)}`
-				});
-			}
-		} else if (req.method == 'OPTIONS') {
-			res.json({
-				status: 200,
-				headers: corsHeaders
+			res.status(200).json({
+				tables: reader?.getTableNames()
 			});
-		} else {
-			res.json({
-				status: 405,
-				headers: corsHeaders,
-				message: 'POST required'
+		} catch (err: unknown) {
+			res.status(400).json({
+				message: `Failed to read .mdb file: ${err instanceof Error ? err.message : String(err)}`
 			});
 		}
+	} else if (req.method == 'OPTIONS') {
+		res.status(200);
+	} else {
+		res.status(405).json({
+			message: 'POST required'
+		});
+	}
+
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Methods", "POST, OPTIONS");
+	res.header("Access-Control-Allow-Headers", "Content-Type");
 });
 
 export default app

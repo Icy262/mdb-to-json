a774e2da1324f3715535516f5e8ddae552198a25
Icy26 <114495528+Icy262@users.noreply.github.com>
2026-06-17 09:03:26 -0400

    Fixed header construction, simplified api logic flow, fixed Buffer construction

--- src/index.ts
+++ src/index.ts
@@ -9,11 +9,23 @@ const __dirname = path.dirname(__filename)
 
 const app = express()
 
-// Example API endpoint - JSON
-app.post('/get-tables', (req, res) => {
-	if (req.method == 'POST') {
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Methods", "POST, OPTIONS");
+	res.header("Access-Control-Allow-Headers", "Content-Type");
+
+	if (req.method === "OPTIONS") {
+		return res.sendStatus(200);
+	}
+
+	next();
+});
+
+app.post(
+	'/get-tables',
+	express.raw({ limit: '50mb' }),
+	(req, res) => {
 		try {
-			express.raw({ limit: '50mb' });
 			const reader = new MDBReader(req.body);
 
 			res.status(200).json({
@@ -24,17 +36,7 @@ app.post('/get-tables', (req, res) => {
 				message: `Failed to read .mdb file: ${err instanceof Error ? err.message : String(err)}`
 			});
 		}
-	} else if (req.method == 'OPTIONS') {
-		res.status(200);
-	} else {
-		res.status(405).json({
-			message: 'POST required'
-		});
 	}
-
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Methods", "POST, OPTIONS");
-	res.header("Access-Control-Allow-Headers", "Content-Type");
-});
+);
 
 export default app
